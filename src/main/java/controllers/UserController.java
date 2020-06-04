336e1845f4b829ea928d1d91118d459113ce13c2
nzodalandry <nzodalandry@gmail.com>
2020-06-04 15:01:21 +0200

    Ajout de UserController

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -5,10 +5,29 @@
  */
 package controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author nzoda
  */
+@RestController
+@RequestMapping("/api")
 public class UserController {
-    
+    @RequestMapping(value = "user/{id}", method = RequestMethod.GET)
+	public @ResponseBody Content getContent(@PathVariable("id") long id) {
+		LogHelper.getLogger().logInfo("calling: /test/ " + id);
+
+		Content content = new Content();
+		content.setId(id);
+		content.setData("paramiter: " + id);
+		content.setDescription("That's works");
+		content.setStatus(200);
+		content.setError(null);
+
+		return content;
+
+	}
 }
